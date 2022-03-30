e27cd249073bbd048260096b73869ba83f70b539
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-03-31 04:54:54 +0800

    class done!

--- app.js
+++ app.js
@@ -1,65 +1,76 @@
 "use strict";
 
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={35525da445f656dff1788bc7bfd5db91}
+const weatherContainer = document.querySelector(".weather");
+const inputBox = document.querySelector(".inputBox");
+const submitBtn = document.querySelector(".submitBtn");
 
-// fetch(
-//   "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={35525da445f656dff1788bc7bfd5db91}"
-// );
+class App {
+  constructor() {
+    submitBtn.addEventListener("click", this._getWeather.bind(this));
 
-// var options = {
-//   enableHighAccuracy: true,
-//   timeout: 5000,
-//   maximumAge: 0,
-// };
-
-// function success(pos) {
-//   const { latitude: lat, longitude: lng } = pos.coords;
-// }
-
-// function error(err) {
-//   console.warn("ERROR(" + err.code + "): " + err.message);
-// }
-
-const getCoords = async () => {
-  try {
-    const pos = await new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
+    inputBox.addEventListener("keyup", function (e) {
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        submitBtn.click();
+      }
     });
-    const coords = [pos.coords.latitude, pos.coords.longitude];
-    return coords;
-  } catch {
-    (err) => console.log(err);
   }
-};
 
-const getCity = async (coords) => {
-  try {
-    const resGeo = await fetch(
-      `https://geocode.xyz/${coords[0]},${coords[1]}?geoit=json`
-    );
-    if (!resGeo.ok) throw new Error("Problem getting location data");
-
-    const dataGeo = await resGeo.json();
-    //   console.log(dataGeo.city);
-    const city = dataGeo.city;
-    return city;
-  } catch {
-    (err) => console.log(err);
-  }
-};
-
-const getWeather = async () => {
-  try {
-    const coords = await getCoords();
-    const city = await getCity(coords);
+  async _getWeather() {
+    if (!inputBox.value) return;
+    // const coords = await this._getCoords();
+    // const city = await this._getCity(coords);
     const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=35525da445f656dff1788bc7bfd5db91`
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&appid=35525da445f656dff1788bc7bfd5db91&units=metric&lang=zh_tw`
     );
     const data = await res.json();
-    console.log(data);
-  } catch {
-    (err) => console.log(err);
+    this._renderWeather(data);
   }
-};
 
-getWeather();
+  _renderWeather(data) {
+    weatherContainer.innerHTML = "";
+
+    const html = `
+      <div class="city__data">
+        <h2 class="name">城市：${data.name}</h2>
+        <h2 class="descriptiion">天氣：${data.weather[0].description}
+        </h2>
+        <h2 class="temp">氣溫：${data.main.temp} °C </h2>
+        <h2 class="feels_like">體感：${data.main.feels_like}
+        <h2>
+        <h2 class="humidity">濕度：${data.main.humidity}
+        </h2>
+        <h2 class='visibility'>能見度：${data.visibility}
+        </h2>
+      </div>
+    `;
+    weatherContainer.insertAdjacentHTML("beforeend", html);
+  }
+
+  // async _getCoords() {
+  //   try {
+  //     const resPos = await new Promise((resolve, reject) => {
+  //       navigator.geolocation.getCurrentPosition(resolve, reject);
+  //     });
+  //     const coords = [resPos.coords.latitude, resPos.coords.longitude];
+  //     return coords;
+  //   } catch {
+  //     (err) => console.log(err);
+  //   }
+  // }
+
+  // async _getCity(coords) {
+  //   try {
+  //     const resGeo = await fetch(
+  //       `https://geocode.xyz/${coords[0]},${coords[1]}?geoit=json`
+  //     );
+  //     if (!resGeo.ok) throw new Error("Problem getting location data");
+  //     const dataGeo = await resGeo.json();
+  //     const city = dataGeo.city.split(" ");
+  //     return city;
+  //   } catch {
+  //     (err) => console.log(err);
+  //   }
+  // }
+}
+const run = new App();
